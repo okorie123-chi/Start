0f46e4be9c1ef916b324c2dca6451b47143cce3b
okorie123-chi <okoriechidiogo20@outlook.com>
2025-02-04 21:22:19 +0100

    Manipulation

---
+++ Overview/MyConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string response;
+Console.WriteLine("What is your name?");
+response = Console.ReadLine();
+Console.WriteLine($"Enjoy the course, {response}!");
+
+OperatingSystem thisos = Environment.OSVersion;
+Console.WriteLine(thisos)
